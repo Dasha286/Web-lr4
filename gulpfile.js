5c579e5d1557e4b9ecc969db8faaee5d1bd9a1e1
Daria <tadladaria@gmail.com>
2025-06-02 09:15:29 +0300

    End version

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const server = require('gulp-server-livereload');
 const clean = require('gulp-clean');
 const fs = require('fs');
 
+
 gulp.task('html',function(){
     return gulp.src('./src/*.html')
         .pipe(fileInclude({
